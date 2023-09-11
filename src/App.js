307ef77d4e9cf4ede967056ec2b96b191129301c
Satwikagrawal <satwikagarwal1@gmail.com>
2023-09-11 18:58:27 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import travel_01 from "./assests/01.jpg";
+import travel_02 from "./assests/travel-02.jpg";
+import travel_03 from "./assests/travel-03.jpg";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Slider from "./components/Slider";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const navbarLinks = [
+    { url: "#", title: "Home" },
+    { url: "#", title: "Trips" },
+    { url: "#", title: "Rewards" },
+  ];
+
+  return<div className="App">
+     <Navbar navbarLinks={navbarLinks} />
+      <Hero imageSrc={travel_01}/>
+      <Slider
+        imageSrc={travel_02}
+        title={"Be an explorer."}
+        subtitle={
+          "Our platform offers a wide variety of unique travel locations!"
+        }
+      />
+      <Slider
+        imageSrc={travel_03}
+        title={"Memories for a lifetime."}
+        subtitle={"Your dream vacation is only a few clicks away."}
+        flipped={true}
+      />
+    </div>;
 }
 
 export default App;
